159c6314f94a5e4ada6cf723c1f376ab0605f802
YUIKA <yuikasunmoon@gmail.com>
2021-06-15 09:13:26 +0900

    add

--- AI_2021_flask-master/main.py
+++ AI_2021_flask-master/main.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from flask import send_from_directory
 
-classes = ["rare","welldone"] #분류할 class명을 정의
+classes = ["rare","well-done"] #분류할 class명을 정의
 
 num_classes = len(classes) #list 길이를 취득하고 정의
 
@@ -26,18 +26,18 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
 
 #학습된 모델을 로드함
-model = load_model('my_model.h5')
+model = load_model('my_model4.h5')
 
 #URL를 받은 HTTP메소드를 지정
 @app.route('/', methods=['GET', 'POST']) 
 def upload_file():
     if request.method == 'POST': #request.method는 페이지에 접속 방식을 탐지하는 기능. POST여부를 판별함.
         if 'file' not in request.files: #파일이 없는 경우
-            flash('ファイルがありません')
+            flash('파일이 없습니다')
             return redirect(request.url) # request된 페이지에 전송
         file = request.files['file']
         if file.filename == '': #파일 명이 없는 겨우　　
-            flash('ファイルがありません')
+            flash('파일이 없습니다')
             return redirect(request.url) # request된 페이지에 전송
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename) #sanitize
@@ -52,13 +52,13 @@ def upload_file():
             #변환한 데이터를 모델에 넘겨 예측함
             result = model.predict(data)[0]
             predicted = result.argmax()
-            pred_answer = "당신의 고기는 " + classes[predicted] + " 입니다!"
+            pred_answer =  classes[predicted] + " 입니다!"
 
             #render_template 인수에 answer=pred_answer 와 전달함으로써 index.html 에 작성한 answer 에 pred_answer 를 대입
             #이 인수에 전달할 html 파일은 temlpates 폴더에 놓아 둘 필요가 있음
-            return render_template("index.html",file = file,answer=pred_answer) 
+            return render_template("index1.html",file = file,answer=pred_answer) 
     #POST 요청이 이루어지지 않을 때는 index.html의 answer에는 아무것도 표시하지 않음
-    return render_template("index.html",answer="") 
+    return render_template("index1.html",answer="") 
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename): # 파일을 표지
@@ -68,6 +68,10 @@ def uploaded_file(filename): # 파일을 표지
 @app.route('/AI_2021_flask-master/templates/home1.html') 
 def link1():
         return render_template('home1.html')
+
+@app.route('/AI_2021_flask-master/templates/home2.html') 
+def link2():
+        return render_template('home2.html')        
 
 #Python 스크립트를 실행할 때만 if __name__== '_main_' : 아래의 처리를 실행시키도록 함
 
